6b1a2dd702675278add30ed44ff3b64ba881e425
Klaas Poortinga <poortinga.k@gmail.com>
2020-01-25 18:20:56 +0100

    Different style

--- plugins/klaasie/backend/assets/tailwind.config.js
+++ plugins/klaasie/backend/assets/tailwind.config.js
@@ -1,13 +1,27 @@
 module.exports = {
-  prefix: 'tw-',
-  theme: {
-      inset: {
-      '0': 0,
-      auto: 'auto',
-      '2': '2rem',
+    prefix: 'tw-',
+    theme: {
+        inset: {
+            '0': 0,
+             auto: 'auto',
+            '2': '2rem',
+        },
+        extend: {
+            colors: {
+                'main': {
+                    100: '#EFF8FE',
+                    200: '#D6EEFD',
+                    300: '#BEE4FB',
+                    400: '#8DD0F9',
+                    500: '#5CBCF6',
+                    600: '#53A9DD',
+                    700: '#377194',
+                    800: '#29556F',
+                    900: '#1C384A',
+                },
+            },
+        }
     },
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    variants: {},
+    plugins: []
 }
